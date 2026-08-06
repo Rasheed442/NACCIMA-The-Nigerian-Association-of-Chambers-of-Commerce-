fcac1a9b02cee10195e877b131abecb68c175378
Rasheed442 <abdulraufrasheed15@gmail.com>
2026-08-06 10:56:31 +0100

    updated title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Solution Title",
-  description: "Solution Title web app",
+  title: "NACCIMA",
+  description: "",
   icons: {
     icon: "/favicon.svg",
   },
